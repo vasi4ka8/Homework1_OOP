624954afb1244353dd379389f33da0aba2cd8e24
Lisa <vasi.markova8@yandex.ru>
2024-09-14 21:59:16 +0300

    Task 3

--- main.py
+++ main.py
@@ -16,17 +16,60 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        if self.grades:
+            total_grades = sum([sum(grades) for grades in self.grades.values()])
+            num_grades = sum([len(grades) for grades in self.grades.values()])
+            return round(total_grades / num_grades, 2)
+        return 0
+
+    def __str__(self):
+        avg_grade = self.average_grade()
+        in_progress = ', '.join(self.courses_in_progress)
+        finished = ', '.join(self.finished_courses)
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за домашние задания: {avg_grade}\n'
+                f'Курсы в процессе изучения: {in_progress}\n'
+                f'Завершенные курсы: {finished}')
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self.average_grade() < other.average_grade()
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
         self.grades = {}
+
+    def average_grade(self):
+        if self.grades:
+            total_grades = sum([sum(grades) for grades in self.grades.values()])
+            num_grades = sum([len(grades) for grades in self.grades.values()])
+            return round(total_grades / num_grades, 2)
+        return 0
+
+    def __str__(self):
+        avg_grade = self.average_grade()
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {avg_grade}')
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+        return self.average_grade() < other.average_grade()
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -44,12 +87,33 @@ class Reviewer(Mentor):
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.finished_courses += ['Введение в программирование']
 
-cool_reviewer  = Reviewer('Some', 'Buddy')
-cool_reviewer .courses_attached += ['Python']
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+best_student.rate_lecturer(cool_lecturer, 'Python', 8)
+
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+print(best_student)
+print()
+print(cool_lecturer)
+print()
+print(cool_reviewer)
+
+another_student = Student('John', 'Doe', 'male')
+another_student.courses_in_progress += ['Python']
+cool_reviewer.rate_hw(another_student, 'Python', 9)
+
+print(best_student > another_student)
